af45ac19a7127d9b2a62fa280bea97accee9fe82
judinus10 <judes.20222403@iit.ac.lk>
2025-05-08 17:34:03 +0530

    repository added

---
+++ Authentication/src/main/java/com/Auth/Authentication/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.Auth.Authentication.repository;
+
+import com.Auth.Authentication.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByUsername(String username);
+}
