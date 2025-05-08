c69561da407d60d3bc8520cb029c9cf436fc49ef
judinus10 <judes.20222403@iit.ac.lk>
2025-05-08 17:35:48 +0530

    service added

--- Authentication/src/main/java/com/Auth/Authentication/config/SecurityConfig.java
+++ Authentication/src/main/java/com/Auth/Authentication/config/SecurityConfig.java
@@ -1,30 +1,30 @@
-// package com.Auth.Authentication.config;
+package com.Auth.Authentication.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.http.HttpEntity;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-// @Configuration
-// public class SecurityConfig<SecurityFilterChain> {
+@Configuration
+public class SecurityConfig<SecurityFilterChain> {
 
-//     @Bean
-//     public SecurityFilterChain filterChain(HttpEntity http) throws Exception {
-//         http
-//             .authorizeHttpRequests()
-//                 .requestMatchers("/css/**").permitAll()
-//                 .requestMatchers("/login").permitAll()
-//                 .anyRequest().authenticated()
-//             .and()
-//                 .formLogin()
-//                 .loginPage("/login")
-//                 .defaultSuccessUrl("/", true)
-//                 .permitAll()
-//             .and()
-//                 .logout()
-//                 .logoutSuccessUrl("/login?logout")
-//                 .permitAll();
-//         return http.build();
-//     }
-// }
+    @Bean
+    public SecurityFilterChain filterChain(HttpEntity http) throws Exception {
+        http
+            .authorizeHttpRequests()
+                .requestMatchers("/css/**").permitAll()
+                .requestMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/", true)
+                .permitAll()
+            .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
+        return http.build();
+    }
+}
