249880b99d97392f297231e738795f8b2675e538
judinus10 <judes.20222403@iit.ac.lk>
2025-05-23 10:49:40 +0530

    added fields

--- Authentication/src/main/java/com/Auth/Authentication/model/User.java
+++ Authentication/src/main/java/com/Auth/Authentication/model/User.java
@@ -15,14 +15,22 @@ public class User {
 
     private String password;
 
+    @Column(unique = true)
     private String email;
 
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
     // Getters & Setters
-    public long getId() { 
+
+    public Long getId() { 
         return id; 
     }
 
-    public void setId(long id) { 
+    public void setId(Long id) { 
         this.id = id; 
     }
 
@@ -48,5 +56,29 @@ public class User {
 
     public void setEmail(String email) { 
         this.email = email; 
+    }
+
+    public String getFirstName() { 
+        return firstName; 
+    }
+
+    public void setFirstName(String firstName) { 
+        this.firstName = firstName; 
+    }
+
+    public String getLastName() { 
+        return lastName; 
+    }
+
+    public void setLastName(String lastName) { 
+        this.lastName = lastName; 
+    }
+
+    public String getPhone() { 
+        return phone; 
+    }
+
+    public void setPhone(String phone) { 
+        this.phone = phone; 
     }
 }
