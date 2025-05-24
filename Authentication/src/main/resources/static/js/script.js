4cde8550391684337f6010431ed2b222d1601377
judinus10 <judes.20222403@iit.ac.lk>
2025-05-24 20:34:17 +0530

    added option for viewing password

--- Authentication/src/main/resources/static/js/script.js
+++ Authentication/src/main/resources/static/js/script.js
@@ -61,3 +61,16 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+// view password
+function togglePassword(id, icon) {
+    const input = document.getElementById(id);
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
